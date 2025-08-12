64fb196cdd69c23114bd2f4c5b58b4158fc5dd95
BAYRESTORE <prbayuu@gmail.com>
2025-08-12 23:52:29 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
 };
