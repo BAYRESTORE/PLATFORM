c8b7b5211b64d1e4732c010899c787c869ce5807
BAYRESTORE <prbayuu@gmail.com>
2025-08-12 15:29:04 +0700

    Delete api directory

--- api/admin/secure-data.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { authGuard } from '../../../shared/middleware/authGuard'
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Logic admin-only disini
-  res.status(200).json({ secretData: 'Ini data rahasia admin' })
-}
-
-export default authGuard(handler, ['admin'])
